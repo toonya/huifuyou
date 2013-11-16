897618bd4b5dcf8faac2e640d17dc2281cbc7d87
toonya <msc5762@me.com>
2013-11-16 18:46:22 +0800

    For branch test

--- js/admin.js
+++ js/admin.js
@@ -2,4 +2,9 @@ jQuery(function($){
 	jQuery('#createuser').find('[for="url"]').closest('.form-field').remove();
 	jQuery('#your-profile').find('.form-table').first().remove();
 	jQuery('[for="url"]').closest('tr').remove();
+
+	// ----------------------------------------
+	// ! for a test
+	// ----------------------------------------
+
 })
