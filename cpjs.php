66d01b8f8ce22f15fd34dd3f53bf48c7e1a74953
toonya <msc5762@qq.com>
2013-11-18 21:22:47 +0800

    新建产品介绍

---
+++ cpjs.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: 产品介绍
+*/
+?>
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			
+			<!--开始代码-->
+            
+            
+            
+            
+            
+            <!--结束代码-->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
