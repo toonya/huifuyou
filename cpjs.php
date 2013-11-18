314b801b2b5a984ede3aa6bf9bc780758bc9a12b
toonya <msc5762@qq.com>
2013-11-18 21:29:16 +0800

    修改基本模板

--- cpjs.php
+++ cpjs.php
@@ -8,15 +8,19 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			
-			<!--开始代码-->
+			<?php while ( have_posts() ) : the_post(); ?>
+				
+            <!--开始代码-->
             
             
-            
+            	<h1><?php the_title();?></h1>
+                
+                <article><?php the_content();?></article>
             
             
             <!--结束代码-->
 
+			<?php endwhile; // end of the loop. ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
