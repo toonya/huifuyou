ec014a69e4e17a44738ae02d7f934b34690f50ce
AliServer <msc5762@qq.com>
2013-11-21 11:34:12 +0800

    fix home & ohter page content display

--- content-page.php
+++ content-page.php
@@ -1,6 +1,9 @@
 <div class="container">
-	<?php //the_content();?>
-        <div class="text-center">
-        	<img class="center" src="<?php echo get_template_directory_uri();?>/images/assets/features.png" />
-        </div>
+	<?php if(is_home()):; ?>
+    <div class="text-center">
+    	<img class="center" src="<?php echo get_template_directory_uri();?>/images/assets/features.png" />
+    </div>
+    <?php else: ?>
+	<?php the_content();?>
+	<?php endif; ?>
 </div>
