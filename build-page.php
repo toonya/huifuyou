51eb51c3b1b03674468d8673548b01df5e41445b
toonya <msc5762@me.com>
2013-11-24 16:24:28 +0800

    add the html of the banner option

--- build-page.php
+++ build-page.php
@@ -9,25 +9,42 @@ add_action( 'admin_menu', 'home_images' );
 function home_images() {
     add_menu_page( '首页图片', '首页图片', 'manage_options', 'homeImageOptions', 'homeImagesPageBuilder', '', 99 );
 }
-function homeImagesPageBuilder() {?>
+function homeImagesPageBuilder() {
+
+?>
 
     <div id="banner-option">
 	    <h1>首页图片管理</h1>
 
 		<!-- Nav tabs -->
-		<ul class="nav nav-tabs" data-toggle="tab">
-		  <li><a href="#home" data-toggle="tab">Home</a></li>
-		  <li><a href="#profile" data-toggle="tab">Profile</a></li>
-		  <li><a href="#messages" data-toggle="tab">Messages</a></li>
-		  <li><a href="#settings" data-toggle="tab">Settings</a></li>
+		<ul class="nav nav-tabs">
+		  <li class="active"><a href="#1" data-toggle="tab">1</a></li>
+		  <li><a href="#2" data-toggle="tab">+</a></li>
 		</ul>
 
 		<!-- Tab panes -->
-		<div class="tab-content">
-		  <div class="tab-pane active" id="home">...1</div>
-		  <div class="tab-pane" id="profile">...2</div>
-		  <div class="tab-pane" id="messages">...3</div>
-		  <div class="tab-pane" id="settings">...4</div>
+		<div class="tab-content container tab-container">
+		  <div class="tab-pane fade in active" id="1">
+			  <div class="col-sm-11">
+			  	<input type="text" class="form-control" id="title" placeholder="输入标题">
+			  	<br />
+					<div class="row control">
+						<div class="col-sm-6">
+							<button type="button" class="btn btn-primary btn-danger btn-block media">选择图片</button>
+						</div>
+						<div class="col-sm-3">
+							<button type="button" class="btn btn-primary btn-block save">保存</button>
+						</div>
+						<div class="col-sm-3">
+							<button type="button" class="btn btn-warning btn-block delete">删除本新闻</button>
+						</div>
+						<div class="preview"></div>
+					</div>
+				</div>
+		  </div>
+		  <div class="tab-pane fade" id="2">
+			  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, exercitationem itaque ipsam culpa eveniet explicabo similique blanditiis quisquam possimus at?
+		  </div>
 		</div>
     </div>
 
--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,17 @@
 jQuery(function($){
+
+	// ----------------------------------------
+	// ! modify options on user page
+	// ----------------------------------------
+
 	jQuery('#createuser').find('[for="url"]').closest('.form-field').remove();
 	jQuery('#your-profile').find('.form-table').first().remove();
-	jQuery('[for="url"]').closest('tr').remove();
+  jQuery('[for="url"]').closest('tr').remove();
+
+	// ----------------------------------------
+	// ! banner options
+	// ----------------------------------------
+
+
+
 })
