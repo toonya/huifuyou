22416fd87fbb962271563fc7b67b67a2c4c66184
toonya <msc5762@me.com>
2013-11-24 20:36:52 +0800

    add item done.

--- build-page.php
+++ build-page.php
@@ -13,39 +13,59 @@ function homeImagesPageBuilder() {
 
 ?>
 
-    <div id="banner-option">
-	    <h1>首页图片管理</h1>
+<div id="banner-option">
+    <h1>首页图片管理</h1>
 
-		<!-- Nav tabs -->
-		<ul class="nav nav-tabs">
-		  <li class="active"><a href="#1" data-toggle="tab">1</a></li>
-		  <li><a href="#2" data-toggle="tab">+</a></li>
-		</ul>
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="#1" data-toggle="tab">1</a></li>
+        <li><a href="#addnew" data-toggle="tab">+</a></li>
+    </ul>
 
-		<!-- Tab panes -->
-		<div class="tab-content container tab-container">
-		  <div class="tab-pane fade in active" id="1">
-			  <div class="col-sm-11">
-			  	<input type="text" class="form-control" id="title" placeholder="输入标题">
-			  	<br />
-					<div class="row control">
-						<div class="col-sm-6">
-							<button type="button" class="btn btn-primary btn-danger btn-block media">选择图片</button>
-						</div>
-						<div class="col-sm-3">
-							<button type="button" class="btn btn-primary btn-block save">保存</button>
-						</div>
-						<div class="col-sm-3">
-							<button type="button" class="btn btn-warning btn-block delete">删除本新闻</button>
-						</div>
-						<div class="preview"></div>
-					</div>
-				</div>
-		  </div>
-		  <div class="tab-pane fade" id="2">
-			  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, exercitationem itaque ipsam culpa eveniet explicabo similique blanditiis quisquam possimus at?
-		  </div>
+    <!-- Tab panes -->
+    <div class="tab-content container tab-container">
+        <div class="tab-pane active" id="1">
+            <div class="col-sm-11">
+                <!-- input area for store data -->
+                <div class="info">
+                    <input type="text" class="form-control title" placeholder="输入标题" />
+                    <input type="hidden" class="form-control url" />
+                </div>
+                <!-- control area -->
+                <div class="row control">
+                    <div class="col-sm-6">
+                        <button type="button" class="btn btn-primary btn-danger btn-block open-media">选择图片</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button type="button" class="btn btn-primary btn-block save">保存</button>
+                    </div>
+                    <div class="col-sm-3">
+                        <button type="button" class="btn btn-warning btn-block delete">删除本新闻</button>
+                    </div>
+                    <div class="preview col-sm-9"></div>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane" id="addnew">
+        	<div class="col-sm-11">
+                <!-- input area for store data -->
+                <div class="info">
+                    <input type="text" class="form-control title" placeholder="输入标题" />
+                    <input type="hidden" class="form-control url" />
+                </div>
+                <!-- control area -->
+                <div class="row control">
+                    <div class="col-sm-6">
+                        <button type="button" class="btn btn-primary btn-danger btn-block open-media">选择图片</button>
+                    </div>
+                    <div class="col-sm-6">
+                        <button type="button" class="btn btn-primary btn-block add">保存新闻</button>
+                    </div>
+                    <div class="preview col-sm-9"></div>
+                </div>
+            </div>
 		</div>
     </div>
+</div>
 
 <?php } ?>
--- js/admin.js
+++ js/admin.js
@@ -1,17 +1,118 @@
 jQuery(function($){
 
+    // ----------------------------------------
+    // ! modify options on user page
+    // ----------------------------------------
+
+    jQuery('#createuser').find('[for="url"]').closest('.form-field').remove();
+    jQuery('#your-profile').find('.form-table').first().remove();
+    jQuery('[for="url"]').closest('tr').remove();
+
+    // ----------------------------------------
+    // ! banner options
+    // ----------------------------------------
+    var banner_uploader;
+    var banner_reciever;
+    var banner_preview;
+
+    jQuery('#banner-option').on('click.openMediaManager','.open-media',function(e){
+
+        // Prevent the default action from occuring.
+        e.preventDefault();
+
+        // Get our Parent element
+        banner_reciever = jQuery(this).closest('.control').siblings('.info').find('.url');
+
+        // preview
+        banner_preview = jQuery(this).closest('.control').find('.preview');
+
+        // If the frame already exists, re-open it.
+        if ( banner_uploader ) {
+            banner_uploader.open();
+            return;
+        }
+        banner_uploader = wp.media.frames.banner_uploader = wp.media({
+
+            //createe our media frame
+            className: 'media-frame banner-media-frame',
+            frame: 'select', //Allow Select Only
+            multiple: false //Disallow Mulitple selections
+        });
+
+        // ----------------------------------------
+        // ! when select
+        // ----------------------------------------
+
+        banner_uploader.on('select', function(){
+            // Grab our attachment selection and construct a JSON representation of the model.
+            var media_attachment = banner_uploader.state().get('selection').map( function( attachment ) {
+                attachment = attachment.toJSON();
+                return attachment;
+            });
+            // Send the attachment URL to our custom input field via jQuery.
+
+            var imgurl = media_attachment[0].url;
+
+            banner_reciever.val(imgurl);
+
+            var $preview = jQuery('<img>').attr('src',imgurl).addClass('img-responsive');
+
+            banner_preview.html($preview);
+
+        });
+
+        // Now that everything has been set, let's open up the frame.
+        banner_uploader.open();
+
+    })
+
 	// ----------------------------------------
-	// ! modify options on user page
+	// ! add new banner item
 	// ----------------------------------------
+	jQuery('#banner-option #addnew').on('click.addBannerItem','.add',function(e) {
+        e.preventDefault();
+        var number = jQuery('.tab-pane').not('#addnew').size()+1;
 
-	jQuery('#createuser').find('[for="url"]').closest('.form-field').remove();
-	jQuery('#your-profile').find('.form-table').first().remove();
-  jQuery('[for="url"]').closest('tr').remove();
+        $newItem = jQuery(this).closest('#addnew').clone();
+        $newItemNav = jQuery('<li>').append('<a href="#'+number+'" data-toggle="tab">'+number+'</a>');
+        addNewInit();
+
+		$newItem.removeClass('active').attr('id',number);
+		$newItem.find('.add').addClass('save').removeClass('add').parent().addClass('col-sm-3').removeClass('col-sm-6').after('<div class="col-sm-3"><button type="button" class="btn btn-warning btn-block delete">删除本新闻</button></div>');
+        console.log($newItem);
+        console.log($newItemNav);
+
+        // insert
+        jQuery('#banner-option .nav-tabs li').last().before($newItemNav);
+        jQuery('#banner-option #addnew').before($newItem);
+	})
+
+	function addNewInit() {
+		jQuery('#banner-option #addnew').find('input').val('').closest('#addnew').find('.preview').html('');
+	}
 
 	// ----------------------------------------
-	// ! banner options
+	// ! delete banner item
 	// ----------------------------------------
+	jQuery('#banner-option .tab-content').on('click.deleteBannerItem','.delete',function(e) {
+        e.preventDefault();
 
 
+    }
 
+    // ----------------------------------------
+    // ! rebuild number
+    // ----------------------------------------
+    function reNumber() {
+	    jQuery('#banner-option .nav-tabs li:not(:last)').each(function(i,e) {
+	    	var number = i+1;
+		    jQuery(e).find('a').attr('href','#'+number).text(number);
+	    });
+	    jQuery('#banner-option .tab-content .tab-pane:not(:last)').each(function(i,e) {
+	    	var number = i+1;
+		    jQuery(e).attr('id',number);
+	    });
+    }
 })
+
+
