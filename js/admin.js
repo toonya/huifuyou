dba9dc0a5ac1f0d4f63926d2c6ee92aa70bfc7a9
toonya <msc5762@me.com>
2013-11-25 11:52:56 +0800

    simplely fix that the admin bar is not showing.

--- js/admin.js
+++ js/admin.js
@@ -3,9 +3,10 @@ jQuery(function($){
     // ! modify options on user page
     // ----------------------------------------
 
-    jQuery('#createuser').find('[for="url"]').closest('.form-field').remove();
-    jQuery('#your-profile').find('.form-table').first().remove();
-    jQuery('[for="url"]').closest('tr').remove();
+    jQuery('#createuser').find('[for="url"]').closest('.form-field').hide();
+    jQuery('#your-profile').find('.form-table').first().hide();
+    jQuery('[name="admin_bar_front"]').attr('checked','checked');
+    jQuery('[for="url"]').closest('tr').hide();
 
     // ----------------------------------------
     // ! banner options
