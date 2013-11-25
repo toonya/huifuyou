2bdf1d408452993025b4528f33f0d92cdc4ccaa0
toonya <msc5762@me.com>
2013-11-25 12:14:11 +0800

    mansy add corporation/security/. modify the download.php

---
+++ corporation.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Template Name: 渠道合作
+*/
+?>
+
+<?php get_header(); ?>
+<style type="text/css">
+h2, .cor-p { width:1000px; 
+     margin:0 auto; 
+	 text-align:left; 	 
+	 font-family:Microsoft YaHei,微软雅黑;
+	 font-weight:bolder;
+}
+.cor-p {
+	color:grey; font-size:16px; padding-bottom:10px;
+}
+h2 { 
+    color:#2f75aa;
+	padding-top:50px; 
+	padding-bottom:10px;  }
+</style>
+	<div id="primary" class="site-content">
+		<div id="content" role="main" style="padding:0;background:white;text-align:center;">
+			<?php while ( have_posts() ) : the_post(); ?>
+				
+            <!--开始代码-->
+                <img class="img-responsitive" 
+                     src="<?php echo get_template_directory_uri()?>/images/corporation/corporation.png">
+                <h2>什么是乐刷商务版</h2>
+                <p class="cor-p">乐刷商务版是一款支持商户进行刷卡收款并通过银联认证的移动手机刷卡器。</p>
+                <img src="<?php echo get_template_directory_uri()?>/images/corporation/1.png">
+                <h2>乐刷商务版优势对比</h2>
+                <img src="<?php echo get_template_directory_uri()?>/images/corporation/2.png">
+                <h2>乐刷商务版便民服务</h2>
+                <img src="<?php echo get_template_directory_uri()?>/images/corporation/3.png">
+                <h2>申请渠道条件</h2>
+                <img src="<?php echo get_template_directory_uri()?>/images/corporation/4.png">
+                <h2>申请流程</h2>
+                <img style="margin-bottom:70px" src="<?php echo get_template_directory_uri()?>/images/corporation/5.png">
+            <!--结束代码-->
+
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
---
+++ security.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: 安全保障
+*/
+?>
+
+<?php get_header(); ?>
+<style type="text/css">
+#feature-1, #feature-2, #feature-3 { width:800px; margin:50px auto; padding-bottom:50px; text-align:left; font-family:Microsoft YaHei,微软雅黑; font-size:30px; border-bottom:2px dashed grey; }
+.font-12 { font-size:12px; }
+</style>
+	<div id="primary" class="site-content">
+		<div id="content" role="main" style="background:#f4f4f4; text-align:center;">
+			<?php while ( have_posts() ) : the_post(); ?>
+				
+            <!--开始代码-->
+                <p id="feature-1">通过银联相关认证<img style="vertical-align:top;margin-left:300px;" src="<?php echo get_template_directory_uri()?>/images/security/unionpay.png"></p>
+                <p id="feature-2">软硬件双重加密<img style="vertical-align:top;margin-left:400px;" src="<?php echo get_template_directory_uri()?>/images/security/2.png"></p>
+                <p id="feature-3" style="border-bottom:none;margin-bottom:0">后台数据严密审核<img style="vertical-align:top;margin-left:365px;" src="<?php echo get_template_directory_uri()?>/images/security/1.png"></p>
+            <!--结束代码-->
+
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
