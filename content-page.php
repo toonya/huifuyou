4665b643917c84ac624074ebfe76e3ecbf2be600
toonya <msc5762@qq.com>
2013-12-04 23:27:42 +0800

    flink

--- content-page.php
+++ content-page.php
@@ -13,14 +13,14 @@
     <div class="flink">
     	<p style="width:900px; margin:0 auto; font-family:Microsoft YaHei,微软雅黑; margin-bottom:10px;">友情链接：</p>
         <ul>
-        	<li class="m-bottom-30"><a href="" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/yeahka.png"></a></li>
-            <li class="m-bottom-30"><a href="" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/unionpay.jpg"></a></li>
-            <li class="m-bottom-30"><a href="" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/bank-3.jpg"></a></li>
-            <li class="m-bottom-30"><a href="" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/bank-4.jpg"></a></li>
-            <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/bank-5.jpg"></a></li>
-            <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/bank-6.jpg"></a></li>
-            <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/bank-1.jpg"></a></li>
-            <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/bank-2.jpg"></a></li>
+        	<li class="m-bottom-30"><a href="http://lepos.yeahka.com/" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/yeahka.png"></a></li>
+            <li class="m-bottom-30"><a href="http://cn.unionpay.com/" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/unionpay.jpg"></a></li>
+            <li class="m-bottom-30"><a href="http://www.cmbchina.com/" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/bank-3.jpg"></a></li>
+            <li class="m-bottom-30"><a href="http://www.boc.cn/" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/bank-4.jpg"></a></li>
+            <li><a href="http://www.bankcomm.com/" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/bank-5.jpg"></a></li>
+            <li><a href="http://www.abchina.com/cn/" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/bank-6.jpg"></a></li>
+            <li><a href="http://www.ccb.com/cn/home/index.html" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/bank-1.jpg"></a></li>
+            <li><a href="http://www.cebbank.com/" target="_blank"><img src="<?php echo get_template_directory_uri();?>/images/assets/flink/bank-2.jpg"></a></li>
         </ul>
     </div>
     <!--end code-->
