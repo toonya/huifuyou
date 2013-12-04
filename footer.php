ad36df45cda1b6b30fda6a8da6279212327ff2f9
toonya <msc5762@qq.com>
2013-12-05 00:06:36 +0800

    677

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
 		<footer style="background:#e0e3e6;padding:20px 0 15px;">
 			<div class="text-center">
-				<p><a href="">关于我们</a> I <a href="">联系我们</a></p>
+				<p><a href="">关于我们</a> I <a href="<?php echo bloginfo('url'); ?>/?page_id=20">联系我们</a></p>
 				<p>版权所有 Copyright(C)2012-2020 郑州汇付友科技有限公司</p>
 			</div>
 			<div class="hide">
