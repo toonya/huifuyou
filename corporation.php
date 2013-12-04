c1e6cee37faff347101cbae2e1a5cff30717553e
toonya <msc5762@me.com>
2013-12-04 23:16:20 +0800

    1

--- corporation.php
+++ corporation.php
@@ -40,7 +40,8 @@ h2 {
                 <h2>申请流程</h2>
                 <img style="margin-bottom:70px" src="<?php echo get_template_directory_uri()?>/images/corporation/5.png">
             <!--结束代码-->
-				<div class="container"><div class="col-sm-8 col-sm-offset-1"><?php the_content(); ?></div></div>
+                <h2>让我们联系您</h2>
+                <div class="container"><div class="col-sm-8 col-sm-offset-1"><?php the_content(); ?></div></div>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
