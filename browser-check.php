2c9ffc50aa30c653f3a3f9109d343c9544b92a59
toonya <msc5762@qq.com>
2013-12-05 13:48:20 +0800

    浏览器下载提示

--- browser-check.php
+++ browser-check.php
@@ -14,8 +14,8 @@ function browser_alert() {
 			</ul>
 			<p>如果你仍然执着于IE，那么请下载一个IE 9版本（不支持xp系统）吧。IE8以下正在渐渐被淘汰。</p>
 			<ul>
-				<li><a target="_blank" href="">IE9(windows7 32位)</a></li>
-				<li><a target="_blank" href="">IE9(windows7 64位)</a></li>
+				<li><a target="_blank" href="http://soft.hao123.com/soft/appid/890.html">IE9(windows7 32位)</a></li>
+				<li><a target="_blank" href="http://soft.hao123.com/soft/appid/895.html">IE9(windows7 64位)</a></li>
 				<li><a target="_blank" href="http://www.baidu.com/s?ie=UTF-8&wd=ie10">ie10</a></li>
 		    </ul>
 			<p>为得到最佳体验效果，360浏览器下请以<span style="color:red">极速模式</span>访问，搜狗浏览器下以<span style="color:red">高速模式</span>访问。在地址栏右侧有模式切换的按钮，点击即可切换浏览模式。</p>
