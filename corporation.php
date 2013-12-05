83f80827528c559cf05d9531b7edf34b455f6655
toonya <msc5762@me.com>
2013-12-05 11:16:23 +0800

    add container to corporation

--- corporation.php
+++ corporation.php
@@ -22,26 +22,28 @@ h2 {
 	padding-bottom:10px;  }
 </style>
 	<div id="primary" class="site-content">
-		<div id="content" role="main" style="padding:0;background:white;">
+		<div id="content" role="main" style="background:white;">
 			<?php while ( have_posts() ) : the_post(); ?>
 
             <!--开始代码-->
-                <img class="img-responsive center-block"
-                     src="<?php echo get_template_directory_uri()?>/images/corporation/corporation.png">
-                <h2>什么是乐刷商务版</h2>
-                <p class="cor-p">乐刷商务版是一款支持商户进行刷卡收款并通过银联认证的移动手机刷卡器。</p>
-                <img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/corporation/1.png">
-                <h2>乐刷商务版优势对比</h2>
-                <img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/corporation/2.png">
-                <h2>乐刷商务版便民服务</h2>
-                <img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/corporation/3.png">
-                <h2>申请渠道条件</h2>
-                <img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/corporation/4.png">
-                <h2>申请流程</h2>
-                <img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/corporation/5.png">
-            <!--结束代码-->
-                <h2>意向合作</h2>
-                <div class="container"><div class="col-xs-8 col-xs-offset-1"><?php the_content(); ?></div></div>
+                <div class="container">
+                	<img class="img-responsive center-block"
+                	     src="<?php echo get_template_directory_uri()?>/images/corporation/corporation.png">
+                	<h2>什么是乐刷商务版</h2>
+                	<p class="cor-p">乐刷商务版是一款支持商户进行刷卡收款并通过银联认证的移动手机刷卡器。</p>
+                	<img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/corporation/1.png">
+                	<h2>乐刷商务版优势对比</h2>
+                	<img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/corporation/2.png">
+                	<h2>乐刷商务版便民服务</h2>
+                	<img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/corporation/3.png">
+                	<h2>申请渠道条件</h2>
+                	<img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/corporation/4.png">
+                	<h2>申请流程</h2>
+                	<img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/corporation/5.png">
+                	            <!--结束代码-->
+                	<h2>意向合作</h2>
+                	<div class="container"><div class="col-xs-8 col-xs-offset-1"><?php the_content(); ?></div></div>
+                </div>
 			<?php endwhile; // end of the loop. ?>
             <!--结束代码-->
 		</div><!-- #content -->
