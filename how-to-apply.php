dda1a451eacc498b9554fcf334023a8baf6e3c9d
toonya <msc5762@me.com>
2013-12-05 10:25:54 +0800

    how to apply responsive revise

--- how-to-apply.php
+++ how-to-apply.php
@@ -11,9 +11,9 @@ p { text-align:center; }
 	<div id="primary" class="site-content">
 		<div id="content" role="main" style="padding:0; background:#f4f4f4; text-align:center;">
 			<?php while ( have_posts() ) : the_post(); ?>
-				
+
             <!--开始代码-->
-              <img class="img-responsitive center-block" src="<?php echo get_template_directory_uri()?>/images/how-to-apply/how-to-apply.jpg">
+              <img class="img-responsive center-block" src="<?php echo get_template_directory_uri()?>/images/how-to-apply/how-to-apply.jpg">
             <!--结束代码-->
 
 			<?php endwhile; // end of the loop. ?>
