b0a2e12ed0005d276f7108f75890c593a38a5e69
toonya <msc5762@qq.com>
2013-12-08 22:42:23 +0800

    ss

--- about-leshua.php
+++ about-leshua.php
@@ -18,7 +18,7 @@ Template Name: 关于乐刷
               <!-- Nav tabs -->
                  <div style="margin-top:50px;" class="col-xs-12 col-md-3">
                     <ul class="nav nav-pills nav-stacked">
-                      <li><a href="#about-pos" data-toggle="tab" style="font-size:28px; font-family:Microsoft YaHei,微软雅黑;">关于POS</a></li>
+                      <li><a href="#about-pos" data-toggle="tab" style="font-size:28px; font-family:Microsoft YaHei,微软雅黑;">关于乐POS</a></li>
                       <li><a href="#about-rate" data-toggle="tab" style="font-size:28px; font-family:Microsoft YaHei,微软雅黑;">关于费率</a></li>
                       <li><a href="#about-three-pay" data-toggle="tab" style="font-size:28px; font-family:Microsoft YaHei,微软雅黑;">关于三方支付</a></li>
                       <li><a href="#about-register" data-toggle="tab" style="font-size:28px; font-family:Microsoft YaHei,微软雅黑;">关于注册申请</a></li>
@@ -28,7 +28,23 @@ Template Name: 关于乐刷
                 <div class="tab-content col-xs-12 col-md-9" style="margin-top:30px;">
                 <!--about-pos开始-->
                   <div class="tab-pane active" id="about-pos">
-
+				  		<p>乐刷商务版简称乐POS，是一款支持商户进行刷卡收款，并通过银联认证的移动手机刷卡器。由深圳移卡科技有限公司研发。</p>
+                        <ul>收单功能：
+                        	<li>普通收款、撤销</li>
+							<li>快递/COD订单号收款、代理商提货收款、保险保单号收款</li>
+                        </ul>
+						<ul>便民服务：
+                            <li>查询余额</li>
+                            <li>信用卡还款</li>
+                            <li>卡卡转账</li>
+                            <li>财付通充值、支付宝充值、手机充值、游戏充值、Q币充值</li>
+                        </ul>
+                        <ul>对账系统：
+                            <li>交易查询</li>
+                            <li>结算查询</li>
+                            <li>经营业绩统计分析</li>
+                            <li>便民服务的收益分成</li>
+                        </ul>
                   </div>
                 <!--//about-pos-->
                 <!--about-rate开始-->
