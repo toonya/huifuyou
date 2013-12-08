4dd249f2db3297d78142a8f0ebee21bdf0cf0153
toonya <msc5762@qq.com>
2013-12-08 23:27:03 +0800

    ss

--- about-leshua.php
+++ about-leshua.php
@@ -20,7 +20,7 @@ Template Name: 关于乐刷
                     <ul class="nav nav-pills nav-stacked">
                       <li><a href="#about-pos" data-toggle="tab" style="font-size:28px; font-family:Microsoft YaHei,微软雅黑;">关于乐POS</a></li>
                       <li><a href="#about-rate" data-toggle="tab" style="font-size:28px; font-family:Microsoft YaHei,微软雅黑;">关于费率</a></li>
-                      <li><a href="#about-three-pay" data-toggle="tab" style="font-size:28px; font-family:Microsoft YaHei,微软雅黑;">关于三方支付</a></li>
+                      <li><a href="#about-three-pay" data-toggle="tab" style="font-size:28px; font-family:Microsoft YaHei,微软雅黑;">关于第三方支付</a></li>
                       <li><a href="#about-register" data-toggle="tab" style="font-size:28px; font-family:Microsoft YaHei,微软雅黑;">关于注册申请</a></li>
                     </ul>
                  </div>
@@ -59,17 +59,27 @@ Template Name: 关于乐刷
                 <!--//about-pos-->
                 <!--about-rate开始-->
                   <div class="tab-pane" id="about-rate">
-
+                  		<p>连锁民生类个体商户或有限公司（包括加油类，超市类，便民类，家用电器销售，手机话费充值）。可以申请0.5%费率。</p>
+                        <p>其他行业可以办理申请0.78%</p>
                   </div>
                 <!--//about-rate-->
                 <!--about-three-pay开始-->
-                  <div class="tab-pane" id="about-rate">
-
+                  <div class="tab-pane" id="about-three-pay">
+                  		<p>深圳市移卡科技有限公司是一家以技术为导向的高科技移动支付公司，针对市场需求，我公司推出三代移动支付终端，第四代终端还在研发中。</p>
+                        <ul>
+                            <li>公司自2011年初成立，即获得著名VC和腾讯的风险投资，注册资本1500万元。</li>
+							<li>初创团队由来自国内知名互联网、通信、金融支付行业企业资深人士组成；</li>
+							<li>拥有深厚的支付行业背景经验和强大的软硬件自主研发能力。</li> 
+                            <li>2011年7月，国内率先发布个人刷卡支付产品；</li>
+							<li>2011年12月，发布“乐刷1.5” ；用户无需开通网银或第三方支付平台帐户，即可通过个人手机和“乐刷1.5 APP”进行刷卡支付； 目前，乐刷移动支付平台已稳健运营，用户飞速增长。</li>
+							<li>持续创新：两款“乐POS ”产品和新一代”乐刷”产品即将发布。</li>
+                        </ul>
+                        <img src="<?php echo get_template_directory_uri()?>/images/about-leshua/5.png">
                   </div>
                 <!--//about-three-pay-->
                 </div>
                 <!--about-register开始-->
-                  <div class="tab-pane" id="about-rate">
+                  <div class="tab-pane" id="about-register">
 
                   </div>
                 <!--//about-register-->               
