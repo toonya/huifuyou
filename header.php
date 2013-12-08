caefb0e0579dfe88415ccab9983917a2cc534dab
toonya <msc5762@me.com>
2013-12-08 19:12:12 +0800

    sitename+pagename

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?php echo bloginfo('blogname'); ?></title>
+        <title><?php if(is_home())echo bloginfo('blogname'); else echo bloginfo('blogname').'|'.get_the_title(); ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap -->
         <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet" media="screen">
@@ -15,7 +15,6 @@
         <?php wp_head(); ?>
     </head>
     <body>
-    	<!-- class="auto-add-bg bg-cover" data-bg-src="<?php echo get_template_directory_uri();?>/images/bgheader.jpg" -->
         <header>
             <div class="siteinfo">
             	<div class="container">
